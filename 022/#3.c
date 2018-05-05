1ec4cd7453dd47c4ac9078eaef9f599fc4f78f7a
Polina Karpova <pln.krpv@gmail.com>
2018-05-05 11:01:00 +0300

    Задача 3

---
+++ 022/#3.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+int bit(int num);
+int main()
+{
+    int number, count;
+    puts("Введите число:");
+    while(scanf("%d", &number) == 1)
+    {
+        count = bit(number);
+        printf("%d бита содержится\r\n", count);
+        puts("Ввидите число еще раз:");
+ 
+    }
+    return 0;
+ 
+}
+int bit(int num)
+{
+    int mask = 1;
+    int count = 0;
+    while(num > 0)
+    {
+        count += num & mask;
+        num >>= 1;
+ 
+    }
+    return count;
+ 
+}
